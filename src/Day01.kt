251b8eafa4657399bdb514c5e27b357ed9a2a0a8
Maaike <maaike@drbreakalot.nl>
2021-12-01 15:44:22 +0100

    Partial windows are actually a mistake

--- src/Day01.kt
+++ src/Day01.kt
@@ -7,7 +7,7 @@ fun main() {
 
     fun part2(input: List<String>): Int {
         return input.asInts().asSequence()
-            .windowed(size = 3, partialWindows = true)
+            .windowed(size = 3)
             .map { it.sum() }
             .windowed(2)
             .count { elements -> elements.last() > elements.first() }
