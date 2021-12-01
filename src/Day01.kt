497fac1332627425af7964f1840359619f3bd813
Maaike <maaike@drbreakalot.nl>
2021-12-01 15:48:48 +0100

    Kotlinify code

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,15 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.asInts().asSequence()
-            .windowed(2)
-            .count { elements -> elements.last() > elements.first() }
+        return input.asInts()
+            .zipWithNext()
+            .count { (first, last) -> last > first }
     }
 
     fun part2(input: List<String>): Int {
-        return input.asInts().asSequence()
-            .windowed(size = 3)
-            .map { it.sum() }
-            .windowed(2)
-            .count { elements -> elements.last() > elements.first() }
+        return input.asInts()
+            .windowed(size = 3) { it.sum() }
+            .zipWithNext()
+            .count { (first, last) -> last > first }
     }
 
     val input = readInput("Day01")
