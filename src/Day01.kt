a5a21f5d21b4aa7b67d683aef95aeab460cb2f57
Maaike <maaike@drbreakalot.nl>
2021-12-01 15:20:26 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.asSequence()
+            .windowed(2)
+            .count { elements -> isLarger(elements.first(), elements.last()) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.asSequence()
+            .windowed(size = 3, partialWindows = true)
+            .map { it.asInts() }
+            .map { it.sum() }
+            .windowed(2)
+            .count { elements -> elements.last() > elements.first() }
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
+
+fun isLarger(previous: String, current: String): Boolean {
+    return current.toInt() > previous.toInt()
+}
+
