ba660f7b43197603ef865c27eb6006acaa3ff5dd
Maaike <maaike@drbreakalot.nl>
2021-12-01 15:33:45 +0100

    convert to ints sooner in day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,14 +1,13 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.asSequence()
+        return input.asInts().asSequence()
             .windowed(2)
-            .count { elements -> isLarger(elements.first(), elements.last()) }
+            .count { elements -> elements.last() > elements.first() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.asSequence()
+        return input.asInts().asSequence()
             .windowed(size = 3, partialWindows = true)
-            .map { it.asInts() }
             .map { it.sum() }
             .windowed(2)
             .count { elements -> elements.last() > elements.first() }
@@ -17,9 +16,5 @@ fun main() {
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
-}
-
-fun isLarger(previous: String, current: String): Boolean {
-    return current.toInt() > previous.toInt()
 }
 
