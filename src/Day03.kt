cec500a21e4dd9d4e068d132b3d89c9961fbf164
Maaike <maaike@drbreakalot.nl>
2021-12-03 15:43:37 +0100

    Day 3 part 2. This feels hacky af:(

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,8 +1,7 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        val measurementSize = input.first().length
-        val gamma = input.map { bitValues(it) }
-            .fold(List(measurementSize) { 0 }) { acc, rate -> List(measurementSize) { index -> acc[index] + rate[index] } }
+
+        val gamma = onesPerPosition(input.map { bitValues(it) })
             .map {
                 if (it > input.size / 2) {
                     1
@@ -21,7 +20,30 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        var possibleOxygen = input.map { bitValues(it) }
+        var possibleCO2 = possibleOxygen
+        for (i in input.first().indices) {
+            if (possibleOxygen.isNotEmpty()) {
+                val onesPerPositionOxygen = onesPerPosition(possibleOxygen)
+                possibleOxygen = if (onesPerPositionOxygen[i] >= possibleOxygen.size / 2.0) {
+                    possibleOxygen.filter { it[i] == 1 }
+                } else {
+                    possibleOxygen.filter { it[i] == 0 }
+                }
+            }
+            if (possibleCO2.isNotEmpty()) {
+                val onesPerPositionCO2 = onesPerPosition(possibleCO2)
+                if (onesPerPositionCO2[i] != possibleCO2.size && onesPerPositionCO2[i] != 0) {
+                    possibleCO2 = if (onesPerPositionCO2[i] >= possibleCO2.size / 2.0) {
+                        possibleCO2.filter { it[i] == 0 }
+                    } else {
+                        possibleCO2.filter { it[i] == 1 }
+                    }
+                }
+            }
+        }
+
+        return intValue(possibleOxygen.first()) * intValue(possibleCO2.first())
     }
 
     val input = readInput("Day03")
@@ -31,6 +53,11 @@ fun main() {
 
 fun bitValues(input: String): List<Int> {
     return input.toCharArray().map { it.digitToInt() }
+}
+
+fun onesPerPosition(input: List<List<Int>>): List<Int> {
+    val measurementSize = input.first().size
+    return input.fold(List(measurementSize) { 0 }) { acc, rate -> List(measurementSize) { index -> acc[index] + rate[index] } }
 }
 
 fun intValue(binary: List<Int>): Int {
