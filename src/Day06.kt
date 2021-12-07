63ee6ae9245833ade22e84da72366fa8800fbf1c
Maaike <maaike@drbreakalot.nl>
2021-12-07 11:06:32 +0100

    Day 6 part 2. Lazy solution but it works

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,18 +1,24 @@
 import java.lang.IllegalArgumentException
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        var ageAndCounts = input.single().split(',').asInts()
+    fun part1(input: List<String>): Long {
+        var ageAndCounts = input.single().split(',').asInts().map { it.toLong() }
             .groupBy { it }
-            .mapValues { (_, value) -> value.size }
+            .mapValues { (_, value) -> value.size.toLong() }
         for (x in 1..80) {
             ageAndCounts = stepDay(ageAndCounts)
         }
         return ageAndCounts.values.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        var ageAndCounts = input.single().split(',').asInts().map { it.toLong() }
+            .groupBy { it }
+            .mapValues { (_, value) -> value.size.toLong() }
+        for (x in 1..256) {
+            ageAndCounts = stepDay(ageAndCounts)
+        }
+        return ageAndCounts.values.sum()
     }
 
     val input = readInput("Day06")
@@ -20,12 +26,12 @@ fun main() {
     println(part2(input))
 }
 
-fun stepDay(ageAndCounts: Map<Int, Int>): Map<Int, Int> {
-    val result = mutableMapOf<Int, Int>()
+fun stepDay(ageAndCounts: Map<Long, Long>): Map<Long, Long> {
+    val result = mutableMapOf<Long, Long>()
     ageAndCounts.forEach { (age, count) ->
         when (age) {
             in 1..8 -> result.merge(age - 1, count) { a, b -> a + b }
-            0 -> {
+            0L -> {
                 result.merge(6, count) { a, b -> a + b }
                 result[8] = count
             }
