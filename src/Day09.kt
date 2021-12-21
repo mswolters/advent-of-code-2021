7c9f8c9b3fcc091c5dd65e48ccc7ef4fcdb746a2
Maaike <maaike@drbreakalot.nl>
2021-12-21 15:09:27 +0100

    Day 9 part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -19,14 +19,46 @@ fun main() {
         }
 
         return readGrid(input).map { value, adjacent -> (adjacent.all { value < it }) to value }
-            .onEach { println(it) }
             .flatten()
             .filter { (isLowest, _) -> isLowest }
             .sumOf { (_, height) -> height + 1 }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        data class Element(val value: Int, val x: Int, val y: Int, var basin: Int) {
+            fun neighbours(grid: List<List<Element>>): List<Element> {
+                val up = if (y == 0) null else grid[y - 1][x]
+                val left = if (x == 0) null else grid[y][x - 1]
+                val right = if (x == grid[y].size - 1) null else grid[y][x + 1]
+                val down = if (y == grid.size - 1) null else grid[y + 1][x]
+                return listOfNotNull(up, left, right, down)
+            }
+
+            override fun toString(): String {
+                return "($value, #$basin)"
+            }
+        }
+        fun readGrid(input: List<String>): List<List<Element>> {
+            return input.map { it.toCharArray()}
+                .mapIndexed { y, line -> line.mapIndexed { x, char -> Element(char.digitToInt(), x, y, 0) } }
+        }
+
+        fun flow(grid: List<List<Element>>, element: Element, basin: Int) {
+            if (element.value == 9) return
+            if (element.basin != 0) return
+            element.basin = basin
+            element.neighbours(grid).forEach { flow(grid, it, basin) }
+        }
+
+        val grid = readGrid(input)
+        var basin = 0
+        return grid.flatten()
+            .onEach { basin++; flow(grid, it, basin) }
+            .groupBy { it.basin }
+            .filterKeys { it != 0 }
+            .values.map { it.size }.sorted()
+            .takeLast(3)
+            .fold(1) { acc, size -> acc * size }
     }
 
     val input = readInput("Day09")
